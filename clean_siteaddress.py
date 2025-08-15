afa271480f873dc1c1a1870ef5cbb6b9cac7bae8
Omoluabi1003 <pakiyogun10@gmail.com>
2025-08-15 13:15:07 -0400

    Refactor address cleaning to use shared utilities

--- clean_siteaddress.py
+++ clean_siteaddress.py
@@ -1,49 +1,90 @@
+"""Clean and standardize the SiteAddress data for PointMatch."""
+
+from typing import Dict, List
+
 import pandas as pd
 
-# Load the raw site address data
-raw_file = 'SiteAddress08152025.csv'
-output_file = 'SiteAddressPtMatch.csv'
+from process_data import FIXED_CODES, date_only, to_upper
+
+
+RAW_FILE = "SiteAddress08152025.csv"
+OUTPUT_FILE = "SiteAddressPtMatch.csv"
 
 # Columns to extract and their mapping to PointMatch schema
-columns_to_keep = {
-    'addrnum': 'NUMBER',
-    'roadpredir': 'PREDIR',
-    'roadname': 'STNAME',
-    'roadtype': 'STSUFFIX',
-    'roadpostdir': 'POSTDIR',
-    'unittype': 'UNITTYPE',
-    'unitid': 'UNITNUM',
-    'postcomm': 'MAILCITY',
-    'postal': 'ZIP',
-    'siteaddid': 'FEATID',
-    'County': 'COUNTY',
-    'municipality': 'JURISDICTION',
-    'created_date': 'EFFDATE'
+COLUMNS_TO_KEEP: Dict[str, str] = {
+    "addrnum": "NUMBER",
+    "roadpredir": "PREDIR",
+    "roadname": "STNAME",
+    "roadtype": "STSUFFIX",
+    "roadpostdir": "POSTDIR",
+    "unittype": "UNITTYPE",
+    "unitid": "UNITNUM",
+    "postcomm": "MAILCITY",
+    "postal": "ZIP",
+    "County": "COUNTY",
+    "municipality": "JURISDICTION",
+    "created_date": "EFFDATE",
 }
 
-# Read the CSV and select necessary columns
-site_df = pd.read_csv(raw_file, dtype=str)[list(columns_to_keep.keys())]
-site_df.rename(columns=columns_to_keep, inplace=True)
+ALL_COLUMNS: List[str] = [
+    "NUMBER",
+    "PREDIR",
+    "STNAME",
+    "STSUFFIX",
+    "POSTDIR",
+    "UNITTYPE",
+    "UNITNUM",
+    "MAILCITY",
+    "ZIP",
+    "ZIP+4",
+    "LAT",
+    "LONG",
+    "FEATID",
+    "COUNTYID",
+    "COUNTY",
+    "JURISDICTION",
+    "FIRECODE",
+    "POLCODE",
+    "EFFDATE",
+    "TDTCODE",
+]
 
-# Ensure all expected columns exist
-all_columns = ['NUMBER', 'PREDIR', 'STNAME', 'STSUFFIX', 'POSTDIR',
-               'UNITTYPE', 'UNITNUM', 'MAILCITY', 'ZIP', 'ZIP+4',
-               'LAT', 'LONG', 'FEATID', 'COUNTYID', 'COUNTY',
-               'JURISDICTION', 'FIRECODE', 'POLCODE', 'EFFDATE', 'TDTCODE']
-for col in all_columns:
-    if col not in site_df:
-        site_df[col] = ''
-    else:
-        site_df[col] = site_df[col].fillna('')
 
-# Drop unit information and keep unique base addresses
-base_cols = ['NUMBER', 'PREDIR', 'STNAME', 'STSUFFIX', 'POSTDIR', 'MAILCITY', 'ZIP']
-site_df['UNITTYPE'] = ''
-site_df['UNITNUM'] = ''
-site_df = site_df.drop_duplicates(subset=base_cols)
+def clean_siteaddress(raw_file: str = RAW_FILE, output_file: str = OUTPUT_FILE) -> None:
+    """Read raw site address CSV and produce a normalized PointMatch file."""
 
-# Reorder columns to match PointMatch schema
-site_df = site_df[all_columns]
+    site_df = pd.read_csv(raw_file, dtype=str)[list(COLUMNS_TO_KEEP.keys())]
+    site_df.rename(columns=COLUMNS_TO_KEEP, inplace=True)
 
-# Save the cleaned data
-site_df.to_csv(output_file, index=False)
+    site_df = site_df.applymap(to_upper)
+    site_df["EFFDATE"] = site_df["EFFDATE"].apply(date_only)
+
+    for col, val in FIXED_CODES.items():
+        site_df[col] = val
+
+    for col in ALL_COLUMNS:
+        if col not in site_df:
+            site_df[col] = ""
+        else:
+            site_df[col] = site_df[col].fillna("")
+
+    base_cols = [
+        "NUMBER",
+        "PREDIR",
+        "STNAME",
+        "STSUFFIX",
+        "POSTDIR",
+        "MAILCITY",
+        "ZIP",
+    ]
+    site_df["UNITTYPE"] = ""
+    site_df["UNITNUM"] = ""
+    site_df = site_df.drop_duplicates(subset=base_cols)
+
+    site_df = site_df[ALL_COLUMNS]
+    site_df.to_csv(output_file, index=False)
+
+
+if __name__ == "__main__":
+    clean_siteaddress()
+
--- process_data.py
+++ process_data.py
@@ -1,47 +1,118 @@
 import csv
 import gzip
 
+
+FIXED_CODES = {
+    'FEATID': '2403646',
+    'COUNTYID': '111',
+    'COUNTY': 'ST. LUCIE',
+    'FIRECODE': '73',
+    'POLCODE': '377',
+}
+
+
+def to_upper(value: str) -> str:
+    return value.upper() if isinstance(value, str) else value
+
+
+def date_only(value: str) -> str:
+    if not value:
+        return ''
+    return value.split('T')[0].split(' ')[0]
+
+
+def build_row(row: dict) -> tuple:
+    return (
+        to_upper(row.get('addrnum', '')),
+        to_upper(row.get('roadpredir', '')),
+        to_upper(row.get('roadname', '')),
+        to_upper(row.get('roadtype', '')),
+        to_upper(row.get('roadpostdir', '')),
+        '',  # UNITTYPE
+        '',  # UNITNUM
+        to_upper(row.get('postcomm', '')),
+        to_upper(row.get('postal', '')),
+        '',  # ZIP+4
+        '',  # LAT
+        '',  # LONG
+        FIXED_CODES['FEATID'],
+        FIXED_CODES['COUNTYID'],
+        FIXED_CODES['COUNTY'],
+        to_upper(row.get('municipality', '')),
+        FIXED_CODES['FIRECODE'],
+        FIXED_CODES['POLCODE'],
+        date_only(row.get('created_date', '')),
+        '',  # TDTCODE
+    )
+
+
+def build_existing_row(row: dict) -> tuple:
+    return (
+        to_upper(row.get('NUMBER', '')),
+        to_upper(row.get('PREDIR', '')),
+        to_upper(row.get('STNAME', '')),
+        to_upper(row.get('STSUFFIX', '')),
+        to_upper(row.get('POSTDIR', '')),
+        '',  # UNITTYPE
+        '',  # UNITNUM
+        to_upper(row.get('MAILCITY', '')),
+        to_upper(row.get('ZIP', '')),
+        '',  # ZIP+4
+        '',  # LAT
+        '',  # LONG
+        FIXED_CODES['FEATID'],
+        FIXED_CODES['COUNTYID'],
+        FIXED_CODES['COUNTY'],
+        to_upper(row.get('JURISDICTION', '')),
+        FIXED_CODES['FIRECODE'],
+        FIXED_CODES['POLCODE'],
+        date_only(row.get('EFFDATE', '')),
+        '',  # TDTCODE
+    )
+
+
 def process_files():
     stlucie_addresses = set()
     with open('StLucie.csv', 'r') as stlucie_file:
-        reader = csv.reader(stlucie_file)
-        header = next(reader)
+        reader = csv.DictReader(stlucie_file)
         for row in reader:
-            stlucie_addresses.add(tuple(row))
+            stlucie_addresses.add(build_existing_row(row))
 
-    with gzip.open('StAddy.csv.gz', 'rt') as staddy_file, gzip.open('PointMatch.csv.gz', 'wt', newline='') as pointmatch_file:
+    with gzip.open('StAddy.csv.gz', 'rt') as staddy_file, gzip.open(
+        'PointMatch.csv.gz', 'wt', newline=''
+    ) as pointmatch_file:
         reader = csv.DictReader(staddy_file)
         writer = csv.writer(pointmatch_file)
 
         # Write header to PointMatch.csv.gz
-        writer.writerow(['NUMBER', 'PREDIR', 'STNAME', 'STSUFFIX', 'POSTDIR', 'UNITTYPE', 'UNITNUM', 'MAILCITY', 'ZIP', 'ZIP+4', 'LAT', 'LONG', 'FEATID', 'COUNTYID', 'COUNTY', 'JURISDICTION', 'FIRECODE', 'POLCODE', 'EFFDATE', 'TDTCODE'])
+        writer.writerow([
+            'NUMBER',
+            'PREDIR',
+            'STNAME',
+            'STSUFFIX',
+            'POSTDIR',
+            'UNITTYPE',
+            'UNITNUM',
+            'MAILCITY',
+            'ZIP',
+            'ZIP+4',
+            'LAT',
+            'LONG',
+            'FEATID',
+            'COUNTYID',
+            'COUNTY',
+            'JURISDICTION',
+            'FIRECODE',
+            'POLCODE',
+            'EFFDATE',
+            'TDTCODE',
+        ])
 
         for row in reader:
-            staddy_address = (
-                row.get('addrnum', ''),
-                row.get('roadpredir', ''),
-                row.get('roadname', ''),
-                row.get('roadtype', ''),
-                row.get('roadpostdir', ''),
-                row.get('unittype', ''),
-                row.get('unitid', ''),
-                row.get('postcomm', ''),
-                row.get('postal', ''),
-                '',  # ZIP+4
-                '',  # LAT
-                '',  # LONG
-                '',  # FEATID
-                '',  # COUNTYID
-                row.get('County', ''),
-                row.get('municipality', ''),
-                '',  # FIRECODE
-                '',  # POLCODE
-                row.get('created_date', ''),
-                ''  # TDTCODE
-            )
-
+            staddy_address = build_row(row)
             if staddy_address not in stlucie_addresses:
                 writer.writerow(staddy_address)
+
 
 if __name__ == '__main__':
     process_files()
